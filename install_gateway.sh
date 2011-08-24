b042f3db2c449f647d7576030a1c2125bf5f9bd9
mcbrowne <mcbrowne@ececa60f-9f63-0410-97c7-ea05888eb260>
2011-08-24 19:15:45 +0000

    Added scripts to install the gateway scripts and to run a command for all gateways.
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/cag/trunk/gateway@7254 ececa60f-9f63-0410-97c7-ea05888eb260

---
+++ install_gateway.sh
@@ -0,0 +1,37 @@
+#!/bin/csh
+#
+# We assume this is run from the same directory it is in.  We want this to be
+# /u1/gateway.
+#
+set mydir = `pwd`
+if ($mydir != "/u1/gateway") then
+    if (-d /u1/gateway) then
+        # If we already have a /u1/gateway, maybe the symbolic link already exists?
+        cd /u1/gateway
+	if ($mydir != `pwd`) then
+	    echo /u1/gateway already exists.  Please remove before running this install.
+#           exit 1
+	    cd $mydir
+	endif
+    else
+        # If we don't have a /u1/gateway, make it a symbolic link to here!
+        if (! -d /u1) echo mkdir /u1
+	echo ln -s $mydir /u1/gateway
+    endif
+endif
+
+set path = ($path /reg/g/pcds/package/epics/3.14/base/current/bin/linux-x86_64)
+
+# Remove old installation, if any.
+csh -c "echo rm -f /etc/init.d/epicscagd* /etc/rc*/*epicscagd*"
+
+foreach i (init.d/epicscagd{,-???})
+    echo ln -s /u1/gateway/$i /etc/$i
+end
+
+foreach i (init.d/epicscagd-???)
+    set ii = `basename $i`
+    foreach j (/etc/rc[345].d)
+	echo ln -s ../$i $j/S99$ii
+    end
+end
