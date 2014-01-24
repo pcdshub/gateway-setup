b3555a42eb9e6da6f7cd677d07cdf406ed1b1403
bhill <bhill@ececa60f-9f63-0410-97c7-ea05888eb260>
2014-01-24 03:53:09 +0000

    Added a script to enable using caget and other tools from the gateway machines
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/cag/trunk/gateway@15552 ececa60f-9f63-0410-97c7-ea05888eb260

---
+++ scripts/epicsenv-pscag.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Setup EPICS command line environment for PCDS gateway machines
+
+# Source standard EPICS environment
+. /reg/g/pcds/setup/epicsenv-3.14.12.sh
+
+# Source gateway port and interface definitions
+. /etc/init.d/epicscagp
+
+export EPICS_CA_MAX_ARRAY_BYTES=20500100
+export EPICS_CA_ADDR_LIST="127.0.0.255 $THZ_BC $FEE_BC $LAS_BC $AMO_BC $SXR_BC $XPP_BC $XCS_BC $CXI_BC $MEC_BC $MCC_GW:5080"
