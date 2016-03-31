bdcb26f956e7f7c77be994d3369e5b031337be4d
bhill <bhill@ececa60f-9f63-0410-97c7-ea05888eb260>
2016-03-31 20:44:13 +0000

    Checking in last nights changes to gateway configuration scripts, along w/ a few older changes that had yet to be committed. Primary change was enabling the cds-det interfaces. Also added the sxr-gmd gateway files to svn.
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/cag/trunk/gateway@24866 ececa60f-9f63-0410-97c7-ea05888eb260

--- scripts/epicsenv-pscag.sh
+++ scripts/epicsenv-pscag.sh
@@ -5,7 +5,22 @@
 . /reg/g/pcds/setup/epicsenv-3.14.12.sh
 
 # Source gateway port and interface definitions
-. /etc/init.d/epicscagp
+. /reg/g/pcds/gateway/scripts/epicscagp
+export EPICS_CA_AUTO_ADDR_LIST=NO
+export EPICS_CA_ADDR_LIST="$DEV_IF1 $DEV_IF2 $DEV_IF3 $DEV_IF4 $MCC_GW:5080"
+#export EPICS_CA_AUTO_ADDR_LIST=YES
+#export EPICS_CA_ADDR_LIST="$MCC_GW:5080"
 
-export EPICS_CA_MAX_ARRAY_BYTES=20500100
-export EPICS_CA_ADDR_LIST="$BC_LIST $MCC_GW:5080 $MCC_GW:5079"
+#export EPICS_CA_ADDR_LIST=$BC_LIST
+#export EPICS_CAS_INTF_ADDR_LIST="$DEV_IF1 $DEV_IF2 $DEV_IF3 $DEV_IF4"
+#export EPICS_CAS_IGNORE_ADDR_LIST=`echo $EPICS_CAS_IGNORE_ADDR_LIST | sed -e "s%${DEV_IF1}%%"`
+#export EPICS_CAS_IGNORE_ADDR_LIST=`echo $EPICS_CAS_IGNORE_ADDR_LIST | sed -e "s%${DEV_IF2}%%"`
+#export EPICS_CAS_IGNORE_ADDR_LIST=`echo $EPICS_CAS_IGNORE_ADDR_LIST | sed -e "s%${DEV_IF3}%%"`
+#export EPICS_CAS_IGNORE_ADDR_LIST=`echo $EPICS_CAS_IGNORE_ADDR_LIST | sed -e "s%${DEV_IF4}%%"`
+
+# Don't need these any more
+#export EPICS_CA_AUTO_ADDR_LIST=YES
+#export EPICS_CA_MAX_ARRAY_BYTES=20500100
+#export EPICS_CA_ADDR_LIST="$IP $BC_LIST $MCC_GW:5080 $MCC_GW:5079"
+#export EPICS_CA_ADDR_LIST="$LO_IP"
+
