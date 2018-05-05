b9aa1386d5571b2e60ef72622e40a3d2458c68f0
bhill <bhill@ececa60f-9f63-0410-97c7-ea05888eb260>
2018-05-05 02:49:02 +0000

    Adding a setup script to help configure rhel7 gateway hosts to be ready to enable and start any gateway.
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/cag/trunk/gateway@30019 ececa60f-9f63-0410-97c7-ea05888eb260

---
+++ scripts/install_systemd_links.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#
+# Create soft links in /usr/lib/systemd/system for all epics gateways
+# 
+
+if [ ! -d /usr/lib/systemd/system ]; then
+	echo "This host is not systemd compatible."
+	exit -1
+fi
+export GW_TOP=/reg/g/pcds/gateway
+if [ ! -e /etc/init.d/epicscagp ]; then
+	echo ln -s /reg/g/pcds/gateway/epicscagp /etc/init.d/epicscagp
+	ln -s /reg/g/pcds/gateway/epicscagp /etc/init.d/epicscagp
+fi
+for i in $GW_TOP/scripts/epicscagd-*;
+do
+	SCRIPT_NAME=`basename $i`
+	if [ -e /usr/lib/systemd/system/$SCRIPT_NAME ]; then
+		continue
+	fi
+    echo ln -s $i /usr/lib/systemd/system/$SCRIPT_NAME;
+    ln -s $i /usr/lib/systemd/system/$SCRIPT_NAME;
+done
+
+echo -e
+echo All epics gateway systemd soft links are installed.
+echo -e
+echo Be sure a gateway is configured for the correct host before enabling via
+echo % sudo systemctl enable epicscagd-amo
+echo  or starting via 
+echo % sudo systemctl start epicscagd-sxr
+echo -e
+echo grep for IF in /usr/lib/systemd/system/epicscagd-xxx before starting it
+echo to make sure its configured for that gateway host.
+echo This example is configured for pscag3
+echo % grep IF /usr/lib/systemd/system/epicscagd-fee-kmono
+echo export EPICS_CAS_INTF_ADDR_LIST= echo \$PSCAG3_IFLIST \\\| sed -e  s%\${FEE_IF3}%%
