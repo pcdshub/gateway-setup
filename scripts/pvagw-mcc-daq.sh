31d78bdc765719b5b508f5b5b2bf7739d1bc38a0
Bruce Hill <bhill@slac.stanford.edu>
2020-11-06 16:49:55 -0800

    Add new mcc-daq gateway splitting off daq PVs from mcc gateway.

---
+++ scripts/pvagw-mcc-daq.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Starts the EPICS PVA gateway
+#
+# chkconfig: - 99 1
+# description: EPICS PVA Gateway
+# processname: pvagw-mcc-daq
+
+# Source function library.
+. /etc/init.d/functions
+. /etc/init.d/epicscagp
+
+gwname=mcc-daq
+gwprocport=40445
+export gwHostNum=4
+
+case "$1" in
+start)
+	pvaStart $gwname $gwprocport
+	exit $?
+	;;
+name)
+	echo "PVA Gateway $gwname runs on pscag$gwHostNum w/ procServ port $gwprocport"
+	;;
+env-only)
+	;;
+*)
+	echo $"Usage: $0 {start|name|env-only}"
+	return 1
+esac
+
+return $?
