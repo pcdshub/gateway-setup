69f6bc5325e729c43eac8ab19fe6513198c6945e
Bruce Hill <bhill@slac.stanford.edu>
2020-12-04 15:20:57 -0800

    Added pvagw-drp.sh and activated drp gateways on pscag3

---
+++ scripts/pvagw-drp.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Starts the EPICS PVA gateway
+#
+# chkconfig: - 99 1
+# description: EPICS PVA Gateway
+# processname: pvagw-drp
+
+# Source function library.
+. /etc/init.d/functions
+. /etc/init.d/epicscagp
+
+gwname=drp
+gwprocport=40180
+export gwHostNum=3
+
+case "$1" in
+start)
+	pvaStart $gwname $gwprocport
+	exit $?
+	;;
+name)
+	echo "PVA Gateway $gwname runs on pscag$gwHostNum w/ procServ port $gwprocport"
+	;;
+env-only)
+	;;
+*)
+	echo $"Usage: $0 {start|name|env-only}"
+	return 1
+esac
+
+return $?
