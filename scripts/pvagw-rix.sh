a66f8106d5bffc2eb5efd7b65b4cf5c0d3220ba0
Bruce Hill <bhill@slac.stanford.edu>
2020-12-05 16:30:25 -0800

    Activated pvagw-rix

---
+++ scripts/pvagw-rix.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Starts the EPICS PVA gateway
+#
+# chkconfig: - 99 1
+# description: EPICS PVA Gateway
+# processname: pvagw-rix
+
+# Source function library.
+. /etc/init.d/functions
+. /etc/init.d/epicscagp
+
+gwname=rix
+gwprocport=40082
+export gwHostNum=3
+
+case "$1" in
+start)
+	pvaStart $gwname $gwprocport
+	exit $?
+	;;
+name)
+	echo "PVA Gateway $gwname runs on pscag$gwHostNum w/ procServ port $gwprocport"
+	;;
+env-only)
+	;;
+*)
+	echo $"Usage: $0 {start|name|env-only}"
+	return 1
+esac
+
+return $?
