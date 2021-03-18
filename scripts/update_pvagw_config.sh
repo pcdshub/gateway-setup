d10a74800e31b6d0bae4add4f954d98b0351197c
Bruce Hill <bhill@slac.stanford.edu>
2021-03-18 15:39:07 -0700

    Add support for new gateway hosts w/ leading 0 in gwHostNum.

--- scripts/update_pvagw_config.sh
+++ scripts/update_pvagw_config.sh
@@ -11,15 +11,17 @@ fi
 export GW_TOP=/cds/group/pcds/gateway
 source $GW_TOP/scripts/epicscagp
 
+# Process each pvagw launch script
 for i in $GW_TOP/scripts/pvagw-*.sh;
 do
 	SCRIPT_NAME=`basename $i`
 	SERVICE_NAME=${SCRIPT_NAME%%.sh}
 	PVAGW_NAME=${SERVICE_NAME/pvagw-/}
 	CLIENT_SUBNET=${PVAGW_NAME/-*/}
+	# Get the env variables from the launch script
 	source $i env-only
 	mkdir -p $pvaLogsDir/$PVAGW_NAME
-    #echo Updating $pvaLogsDir/$PVAGW_NAME/pvagw-$PVAGW_NAME.json for host pscag$gwHostNum.
+	#echo Updating $pvaLogsDir/$PVAGW_NAME/pvagw-$PVAGW_NAME.json for host pscag$gwHostNum.
 	python $GW_TOP/scripts/genPvaConf.py --template $GW_TOP/scripts/pvagw-template.json  --name $PVAGW_NAME --gwNum $gwHostNum --hutch $CLIENT_SUBNET
 done
 echo "All PVA gateway config files are uptodate."
