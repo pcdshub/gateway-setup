3630a9febfdec5b74f5082ed236952639f082c8b
Ken Lauer <klauer@slac.stanford.edu>
2021-08-12 09:52:47 -0700

    Uncommitted changes on Thu Aug 12 09:52:47 PDT 2021

--- scripts/pvagw-tst.sh
+++ scripts/pvagw-tst.sh
@@ -16,8 +16,14 @@ export gwHostNum=06
 
 case "$1" in
 start)
-	pvaStart $gwname $gwprocport
-	exit $?
+	# Make sure we're on the right host
+	CUR_HOST=`hostname -s`
+	if [ "$CUR_HOST" == "pscag$gwHostNum" ]; then
+		pvaStart $gwname $gwprocport
+	else
+		echo "Wrong host!"
+		echo "Please start PVA Gateway $1 on pscag$gwHostNum"
+	fi
 	;;
 name)
 	echo "PVA Gateway $gwname runs on pscag$gwHostNum w/ procServ port $gwprocport"
@@ -26,7 +32,5 @@ env-only)
 	;;
 *)
 	echo $"Usage: $0 {start|name|env-only}"
-	return 1
 esac
 
-return $?
