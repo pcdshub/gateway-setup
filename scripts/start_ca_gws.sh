f36cdef41062edfe78697794c40a3ec12a62c1ff
Ken Lauer <klauer@slac.stanford.edu>
2021-08-12 10:04:28 -0700

    Scripts and gitignore update

---
+++ scripts/start_ca_gws.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#
+# Start all CA gateways that are configured for the current gateway host.
+# 
+
+if [ ! -d /usr/lib/systemd/system ]; then
+	echo "This host is not systemd compatible."
+	exit -1
+fi
+if [ $UID != 0 ]; then
+	echo Please run this script as root via sudo.
+	exit 1
+fi
+
+CUR_HOST=`hostname -s`
+export GW_TOP=/cds/group/pcds/gateway
+
+for s in $GW_TOP/scripts/epicscagd-*;
+do
+	SCRIPT_NAME=`basename $s`
+	SERVICE_NAME=${SCRIPT_NAME}
+	GW_HOST_STMT=`grep gwHostNum= $s`
+	gwHostNum=`echo $GW_HOST_STMT | sed -e 's/.*gwHostNum=//'`
+	if [ "$CUR_HOST" != "pscag$gwHostNum" ]; then
+		#echo "$SERVICE_NAME runs on pscag$gwHostNum"
+		continue;
+	fi
+	if [ ! -e /usr/lib/systemd/system/$SERVICE_NAME.service ]; then
+		echo "/usr/lib/systemd/system/$SERVICE_NAME.service  not found!"
+		continue
+	fi
+	if systemctl is-active --quiet $SERVICE_NAME; then
+		echo $SERVICE_NAME is already running.
+		continue
+	fi
+	systemctl enable $SERVICE_NAME
+	systemctl start  $SERVICE_NAME
+	echo Started $SERVICE_NAME
+done
+
