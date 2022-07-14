ce89e50fe5529e5de4645a77e10b0721e52b4542
Bruce Hill <bhill@slac.stanford.edu>
2022-07-13 18:32:47 -0700

    Updates for facued and uedfac to avoid duplicate path for UED hosts which see both networks directly.

---
+++ scripts/pvagw-uedfac-cam.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Starts the EPICS PVA gateway
+#
+# chkconfig: - 99 1
+# description: EPICS PVA Gateway
+# processname: pvagw-xpp
+
+# Source function library.
+. /etc/init.d/functions
+. /cds/group/pcds/gateway/scripts/epicscagp
+
+#export EPICS_PVA_SERVER_PORT=5075?
+gwname=uedfac-cam
+gwprocport=40203
+export gwHostNum=-ued
+
+case "$1" in
+start)
+	pvaStart $gwname $gwprocport
+	exit $?
+	;;
+name)
+	echo "PVA Gateway $gwname runs on pscag$gwHostNum w/ procServ port $gwprocport"
+	;;
+env-only)
+	;;
+*)
+	echo $"Usage: $0 {start|name|env-only}"
+	return 1
+esac
+
+return $?
